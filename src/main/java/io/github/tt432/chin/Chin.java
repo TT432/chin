59c5e69fad036dd4ebabe2dd59ffe3458bd761a2
TT432 <2437315224@qq.com>
2024-07-27 19:09:28 +0800

    :tada: init

---
+++ src/main/java/io/github/tt432/chin/Chin.java
@@ -0,0 +1,11 @@
+package io.github.tt432.chin;
+
+import net.neoforged.fml.common.Mod;
+
+/**
+ * @author TT432
+ */
+@Mod(Chin.MOD_ID)
+public class Chin {
+    public static final String MOD_ID = "chin";
+}
